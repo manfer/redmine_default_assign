a4b8adf197fec2f8e4f29b58c6e61f56b8816070
Fernando San Julián <fjsanjulian@hotmail.com>
2012-12-02 13:20:21 +0100

    Show asignee select on projects form only if there is someone to assign to

--- lib/default_assign/hooks/default_assign_projects_hooks.rb
+++ lib/default_assign/hooks/default_assign_projects_hooks.rb
@@ -8,12 +8,14 @@ class DefaultAssignProjectHook < Redmine::Hook::ViewListener
     selected ||= Setting.plugin_redmine_default_assign['default_assignee_id'] unless Setting.plugin_redmine_default_assign['default_assignee_id'].blank? or 
 										      not context[:project].assignable_users.member?(User.find(Setting.plugin_redmine_default_assign['default_assignee_id']).id)
     selected ||= nil
-    return content_tag(:p, context[:form].select(:default_assignee_id,  
-      options_from_collection_for_select(context[:project].assignable_users, :id, :name,
-      unless selected.blank?
-        Integer(selected)
-      end
-      ), {:include_blank => :none}))
+    if context[:project].assignable_users.any?
+      return content_tag(:p, context[:form].select(:default_assignee_id,  
+        options_from_collection_for_select(context[:project].assignable_users, :id, :name,
+        unless selected.blank?
+          Integer(selected)
+        end
+        ), {:include_blank => :none}))
+    end
  
   end
 
